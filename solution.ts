88aedfb93b8f140a6d5ebff352cf31574c1ea110
Rahmot15 <mdrahmatulla666@gmail.com>
2025-11-26 16:22:00 +0600

    Solve Problem 4: filterByRating

--- solution.ts
+++ solution.ts
@@ -31,3 +31,11 @@ class Person {
     return `Name: ${this.name}, Age: ${this.age}`;
   }
 }
+
+type Item = {
+  title: string;
+  rating: number;
+};
+function filterByRating(items: Item[]): Item[] {
+  return items.filter((item) => item.rating >= 4);
+}
