18ae5731328cbb77898a80745aca53cdca00d150
Rahmot15 <mdrahmatulla666@gmail.com>
2025-11-26 15:03:31 +0600

    Problem 1

---
+++ solution.ts
@@ -0,0 +1,15 @@
+type ValueType = string | number | boolean;
+function formatValue(value: ValueType): ValueType {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
+}
+
+
+
+
+
